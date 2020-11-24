8ecfd44ee455cbbf3dd5f1d6194b5c3ec920527a
Takashi Minohara <minohara@cs.takushoku-u.ac.jp>
2020-11-24 19:06:03 +0900

    starter code

---
+++ three0801.js
@@ -0,0 +1,81 @@
+//
+// 応用プログラミング 課題11 (three0801.js)
+// $Id$
+//
+"use strict"; // 厳格モード
+
+// ライブラリをモジュールとして読み込む
+import * as THREE from "./js/three.module.js";
+import * as dat from "./js/dat.gui.module.js";
+import {CSS3DRenderer, CSS3DObject} from "./js/CSS3DRenderer.js"
+
+function init() {
+  // カメラ位置のパラメータ
+  const controls = {
+    x: 0,
+    y: 0,
+    z: 500
+  };
+
+  // シーン作成
+  const scene = new THREE.Scene();
+
+  // カメラ作成
+  const camera = new THREE.PerspectiveCamera(
+    50, window.innerWidth / window.innerHeight, 1, 5000);
+  cameraUpdate();
+
+  // カメラ位置の更新
+  function cameraUpdate() {
+    camera.position.set(controls.x, controls.y, controls.z);
+    camera.lookAt(0, 0, 0);
+    camera.updateProjectionMatrix();
+  }
+
+  // GUIコントローラの設定
+  const gui = new dat.GUI();
+  {
+    gui.add(controls, "x", -1000, 1000, 50).onChange(cameraUpdate);
+    gui.add(controls, "y", -1000, 1000, 50).onChange(cameraUpdate);
+    gui.add(controls, "z", -1000, 1000, 50).onChange(cameraUpdate);
+  }
+
+  // レンダラ設定
+  const cssRenderer = new CSS3DRenderer();
+  cssRenderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById("WebGL-output").appendChild(cssRenderer.domElement);
+
+  // div要素を作る
+  const div = document.createElement( 'div' );
+  div.style.width = "640px";
+  div.style.height = "360px";
+  div.style.backgroundColor = '#000';
+  // iframe要素を作る
+  const iframe = document.createElement("iframe");
+  iframe.style.width = "640px";
+  iframe.style.height = "360px";
+  iframe.style.border = "0px";
+  iframe.src="https://feng.takushoku-u.ac.jp/course/cs/";
+  div.appendChild( iframe );
+
+  // CSS3Dオブジェクトを作って，シーンに追加
+  const object = new CSS3DObject( div );
+  object.position.set( 0, 0, 0 );
+  scene.add(object);
+
+  // Windowのサイズを変更したときの処理
+  window.addEventListener("resize", ()=>{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight);
+  }, false );
+
+  // アニメーション
+  function update() {
+    cssRenderer.render(scene, camera);
+    requestAnimationFrame(update);
+  }
+  update();
+}
+
+document.onload = init();
---
+++ three0802.js
@@ -0,0 +1,135 @@
+//
+// 応用プログラミング 課題11 (three0801.js)
+// $Id$
+//
+"use strict"; // 厳格モード
+
+// ライブラリをモジュールとして読み込む
+import * as THREE from "./js/three.module.js";
+import * as dat from "./js/dat.gui.module.js";
+import {CSS3DRenderer, CSS3DObject} from "./js/CSS3DRenderer.js"
+
+// ３Ｄページ作成関数の定義
+function init() {
+  // 制御変数の定義
+  const param = {w:0.6, h:0.1, d:0.6,
+     nRow:7, nCol:11, gapX:0.1, gapY:0.2, gapZ:0.4};
+
+  // シーン作成
+  const scene = new THREE.Scene();
+
+  // カメラの作成
+  const camera = new THREE.PerspectiveCamera(
+    50, window.innerWidth/window.innerHeight, 0.1, 1000);
+  {
+    camera.position.set(0,2,15);
+    camera.lookAt(0,0,0);
+  }
+  // 第2のカメラ
+
+  // レンダラの設定
+  /*
+  const renderer = new THREE.WebGLRenderer({alpha:true});
+  {
+    renderer.setClearColor(0x204060);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.style.position = "absolute";
+    renderer.domElement.style.zIndex = 0;
+    renderer.domElement.style.top = 0;
+  }
+  */
+  // 第2のレンダラ
+
+  // Css3Dレンダラ
+  const cssRenderer = new CSS3DRenderer();
+  {
+    cssRenderer.setSize(window.innerWidth, window.innerHeight);
+    cssRenderer.domElement.style.position = "absolute";
+    cssRenderer.domElement.style.zIndex = 1;
+    cssRenderer.domElement.style.top = 0;
+  }
+
+  // 光源の設定
+  { // 環境ライト
+    const light = new THREE.AmbientLight();
+    light.intensity=0.4;
+    scene.add(light);
+  }
+  { // ポイントライト
+    const light = new THREE.PointLight();
+    light.position.set(0, 2, 0);
+    scene.add(light);
+  }
+
+  // 3D物体
+  // スクリーン
+  /*
+  const screen = new THREE.Mesh(
+    new THREE.PlaneGeometry(8.0, 4.5),
+    new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      opacity: 0.0,
+      side: THREE.DoubleSide
+    })
+  )
+  screen.position.set(0,0,0);
+  scene.add(screen);
+  */
+  // 椅子
+
+  // 座席の生成
+
+  // Markerの生成
+
+  // CSS3D表示のための設定
+    // div要素の生成
+  const div = document.createElement( 'div' );
+  div.style.width = "640px";
+  div.style.height = "360px";
+  div.style.backgroundColor = '#000';
+  // iframe要素の生成
+  const iframe = document.createElement("iframe");
+  iframe.style.width = "640px";
+  iframe.style.height = "360px";
+  iframe.style.border = "0px";
+  iframe.src = "https://www.youtube.com/embed/w23RIKTYF28?version=3"
+      +"&mute=1&autoplay=1&controls=0"
+      +"&loop=1&playlist=-9pMuSNlN6A";
+  div.appendChild( iframe );
+  // CSSオブジェクトの生成
+  const cssObject = new CSS3DObject(div);
+  cssObject.scale.x *= 8/640;
+  cssObject.scale.y *= 8/640;
+  //cssObject.position.copy(screen.position);
+  //cssObject.rotation.copy(screen.rotation);
+  // CSSシーンの生成
+  const cssScene = new THREE.Scene();
+  cssScene.add(cssObject);
+
+  // レンダラーの配置
+  document.getElementById("CSS3D-output").appendChild(cssRenderer.domElement);
+//  cssRenderer.domElement.appendChild(renderer.domElement);
+
+  // シート選択のための設定
+
+  // Windowサイズの変更処理
+  window.addEventListener("resize", ()=>{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+//    renderer.setSize( window.innerWidth, window.innerHeight );
+    cssRenderer.setSize( window.innerWidth, window.innerHeight );
+  }, false );
+
+  // 描画処理
+  function update(time) {
+//    renderer.render(scene, camera);
+    cssRenderer.render(cssScene, camera);
+    requestAnimationFrame(update);
+  }
+
+  // 描画開始
+  requestAnimationFrame(update);
+}
+
+document.onload = init();
